96c92443fc1c153f35375917d97456f23109a084
xiao <>
2024-02-21 03:28:39 +0800

    add pkg

--- file.go
+++ file.go
@@ -2,16 +2,15 @@ package loader
 
 import (
 	"go/ast"
-
-	"golang.org/x/tools/go/packages"
 )
 
-type (
-	Package = packages.Package
-	File    struct {
-		File     *ast.File
-		Pkg      *Package
-		Filename FileName
-		GenBy    string
-	}
-)
+type File struct {
+	File     *ast.File
+	Pkg      *Package
+	Filename FileName
+	GenBy    string
+}
+
+func (f *File) Package() Pkg {
+	return MkPkg(f.Pkg)
+}
---
+++ pkg.go
@@ -0,0 +1,111 @@
+package loader
+
+import (
+	"bytes"
+	"flag"
+	"go/ast"
+	"go/printer"
+	"go/token"
+	"go/types"
+	"os"
+	"strings"
+
+	"golang.org/x/tools/go/packages"
+	"golang.org/x/tools/go/types/typeutil"
+)
+
+// https://stackoverflow.com/questions/14249217/how-do-i-know-im-running-within-go-test
+var runningWithGoTest = flag.Lookup("test.v") != nil ||
+	strings.HasSuffix(os.Args[0], ".test")
+
+type Package = packages.Package
+
+type Pkg struct {
+	*Package
+}
+
+func MkPkg(p *Package) Pkg {
+	return Pkg{p}
+}
+
+func (p *Pkg) ShowNode(n ast.Node) string {
+	show := func() string {
+		var buf bytes.Buffer
+		_ = printer.Fprint(&buf, p.Fset, n)
+		return buf.String()
+	}
+	fset := p.Fset
+	if runningWithGoTest {
+		return show()
+	}
+	return show() + "\nat " + fset.Position(n.Pos()).String()
+}
+
+func (p *Pkg) TypeInfo() *types.Info               { return p.TypesInfo }
+func (p *Pkg) ObjectOf(id *ast.Ident) types.Object { return p.TypeInfo().ObjectOf(id) }
+func (p *Pkg) TypeOf(e ast.Expr) types.Type        { return p.TypeInfo().TypeOf(e) }
+func (p *Pkg) Callee(call *ast.CallExpr) types.Object {
+	return typeutil.Callee(p.TypeInfo(), call)
+}
+
+func (p *Pkg) UpdateType(e ast.Expr, t types.Type) {
+	p.TypeInfo().Types[e] = types.TypeAndValue{Type: t}
+}
+
+func (p *Pkg) UpdateUses(idOrSel ast.Expr, obj types.Object) {
+	info := p.TypeInfo()
+	switch x := idOrSel.(type) {
+	case *ast.Ident:
+		info.Uses[x] = obj
+	case *ast.SelectorExpr:
+		info.Uses[x.Sel] = obj
+	default:
+		panic("unreached")
+	}
+}
+
+func (p *Pkg) UpdateDefs(idOrSel ast.Expr, obj types.Object) {
+	info := p.TypeInfo()
+	switch x := idOrSel.(type) {
+	case *ast.Ident:
+		info.Defs[x] = obj
+	case *ast.SelectorExpr:
+		info.Defs[x.Sel] = obj
+	default:
+		panic("unreached")
+	}
+}
+
+func (p *Pkg) CopyTypeInfo(new, old ast.Expr) {
+	info := p.TypeInfo()
+	//goland:noinspection GoReservedWordUsedAsName
+	switch new := new.(type) {
+	case *ast.Ident:
+		orig := old.(*ast.Ident)
+		if obj, ok := info.Defs[orig]; ok {
+			info.Defs[new] = obj
+		}
+		if obj, ok := info.Uses[orig]; ok {
+			info.Uses[new] = obj
+		}
+
+	case *ast.SelectorExpr:
+		orig := old.(*ast.SelectorExpr)
+		if sel, ok := info.Selections[orig]; ok {
+			info.Selections[new] = sel
+		}
+	}
+
+	if tv, ok := info.Types[old]; ok {
+		info.Types[new] = tv
+	}
+}
+
+func (p *Pkg) NewIdent(name string, t types.Type) *ast.Ident {
+	ident := ast.NewIdent(name)
+	p.UpdateType(ident, t)
+
+	obj := types.NewVar(token.NoPos, p.Types, name, t)
+	p.UpdateUses(ident, obj)
+	return ident
+}
